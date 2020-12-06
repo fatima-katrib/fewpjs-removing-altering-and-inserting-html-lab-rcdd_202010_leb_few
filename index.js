2f107bef54feb376bcbc7177935b3ffec147f71e
jovial-application-1943 <fatima.katrib@lau.edu>
2020-12-06 18:00:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let main=document.querySelector("main#main");
 main.remove();
 
 
-let newHeader=document.createElement('h1','');
+let newHeader=document.createElement('h1');
+newHeader.id="victory";
 
 
